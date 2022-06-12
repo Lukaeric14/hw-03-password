4b5f793eb5c1cf392ffd46338d7639b220c2028d
Luka <lukaericnu@gmail.com>
2022-06-12 12:31:48 -0500

    final commit

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
-let lowercaseChar = ["a", "b", "c", "d"];
-let uppercaseChar = ["A", "B", "C"];
-let numericChar = ["1","2","3"];
-let specialChar = ["!","@","#"];
+let lowercaseChar = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let uppercaseChar = ["A", "B", "C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let numericChar = ["1","2","3","4","5","6","7","8","9","0"];
+let specialChar = ["!","@","#","$","%","^","&","*","(",",",")"];
 let possibleChar = [];
 let finalPass = [];
 
@@ -15,6 +16,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+function generatePassword() {
 
   var passLength = window.prompt("How long would you like your password to be? 10 Characters Atleast");
   if (passLength <= 9 || passLength == "") {
@@ -26,8 +30,9 @@ function writePassword() {
   var passNumeric = window.confirm("Should your password include numeric characters?");
   var passSpecial = window.confirm("Should your password include special characters?");
 
-console.log(passLength,passLowerCase,passUpperCase,passNumeric,passSpecial);
-function generatePassword() {
+  console.log(passLength,passLowerCase,passUpperCase,passNumeric,passSpecial);
+    possibleChar = [];
+    finalPass = [];
 
   if (passLowerCase === true) {
     possibleChar = possibleChar.concat(lowercaseChar);
@@ -45,19 +50,17 @@ function generatePassword() {
     possibleChar = possibleChar.concat(specialChar)
   }
 
-  console.log(possibleChar);
+  console.log("Possible Characters:" + possibleChar);
 
  for (let i = 0; i < passLength; i++){
-  
    var currentPosition = Math.floor(Math.random()*possibleChar.length);
    var additionalChar = possibleChar[currentPosition];
    finalPass.push(additionalChar);
    console.log(finalPass);
  }
-}
-  possibleChar = [];
-  finalPass = [];
-
+ let ffinalPass = finalPass.join('');
+ console.log(ffinalPass);
+ return ffinalPass
 }
 
 // Add event listener to generate button
